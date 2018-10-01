2021de2ddcb2f75174944185d9c3ccc2778fb13f
naoki1510 <naoki1510@shallweband.net>
2018-10-01 12:49:41 +0900

    Block can be configurable.

--- src/surva/hotblock/tasks/PlayerCoinGiveTask.php
+++ src/surva/hotblock/tasks/PlayerCoinGiveTask.php
@@ -32,7 +32,7 @@ class PlayerCoinGiveTask extends Task {
         foreach($gameLevel->getPlayers() as $playerInLevel) {
             $blockUnderPlayer = $gameLevel->getBlock($playerInLevel->subtract(0, 0.5));
 
-            if($blockUnderPlayer->getId() === Block::QUARTZ_BLOCK) {
+            if($blockUnderPlayer->getId() === Item::fromString($this->getConfig()->get('moneyblock', Block::QUARTZ_BLOCK))->getId()) {
                 if(count($gameLevel->getPlayers()) < $this->getHotBlock()->getConfig()->get("players", 2)) {
                     $playerInLevel->sendTip(
                         $this->getHotBlock()->getMessage(
